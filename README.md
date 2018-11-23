a92b3f6ad97972a9823e16565f6fd3d8eac24792
= <=>
2018-11-24 00:23:02 +0200

    More fizzbuzz solving posibilities

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Need to have Python installed on machine.
 
 Cmd or Shell(in directory where fizzbuzz.py is located):
 
+Calling fizzbuzz function
+
+<h3><b>If on windows use "" quotes </b></h3>
+
+<b>python -c 'from fizzbuzz import *; fizz_with_range()'</b> 
+
+or method call from class
+
 <b>python -c 'from fizzbuzz import *; new_buzz.{function_from_list}'</b> 
 
 List of functions:
@@ -15,13 +23,18 @@ List of functions:
 fizz_with_binary() <br />
 fizz_with_range() <br />
 fizz_with_while() <br />
-fizz_with_function_calls() <br />
 fizz_with_recursion(1) <br /> 
+fizz_with_static_method() <br />
+fizz_with_class_method() <br />
 
-or class contructor call
+or class constructor call
 
 python -c 'from fizzbuzz import *; new_buzz = FizzBuzz(True)' 
 
 or class in class constructor call
 
 python -c 'from fizzbuzz import *; new_buzz.Fizzbuzzer(True)'
+
+accesing fizzbuzz as class argument
+
+python -c 'from fizzbuzz import *; new_buzz.fizz_argument'
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,20 @@
-
 class FizzBuzz:
 
     # After creating new instance of FizzBuzz constructor runs immediately if passed argument is True
     def __init__(self, run_or_not_to_run=False):
 
-        if (run_or_not_to_run):
+        if run_or_not_to_run:
             i = 1
             while i < 101:
-                print("FizzBuzz") if i % 3 == 0 and i % 5 == 0 else print("Fizz") \
-                    if i % 3 == 0 else print("Buzz") if i % 5 == 0 else print(i)
+                print("FizzBuzz") if i % 15 == 0 \
+                    else print("Fizz") if i % 3 == 0 \
+                    else print("Buzz") if i % 5 == 0 \
+                    else print(i)
                 i += 1
+        #this will run code them class is iniatilazed
+        self.fizz_argument = fizz_with_range()
 
-    # creating Fizzbuzzer class inside FizzBuzz class also argument for running code thought constructor
+    # creating Fizzbuzzer class inside FizzBuzz class also argument for running code throught constructor
     # class can be set to True
     class Fizzbuzzer:
 
@@ -20,9 +23,26 @@ class FizzBuzz:
             if run_or_not_to_run:
                 i = 1
                 while i < 101:
-                    print("FizzBuzz") if i % 3 == 0 and i % 5 == 0 else print("Fizz") \
-                        if i % 3 == 0 else print("Buzz") if i % 5 == 0 else print(i)
+                    print("FizzBuzz") if i % 15 == 0 \
+                        else print("Fizz") if i % 3 == 0 \
+                        else print("Buzz") if i % 5 == 0 \
+                        else print(i)
                     i += 1
+
+    #class method can uses FizzBuzz class methods without self
+    @classmethod
+    def fizz_with_class_method(cls):
+        return fizz_with_range()
+
+    #static method independant from class method, but can be accesed throught class
+    @staticmethod
+    def fizz_with_static_method():
+
+        for i in range(1, 101):
+            print("FizzBuzz") if i % 15 == 0 \
+                else print("Fizz") if i % 3 == 0 \
+                else print("Buzz") if i % 5 == 0 \
+                else print(i)
 
     #-------------FizzBuzz Functions------------------
 
@@ -30,7 +50,7 @@ class FizzBuzz:
     def fizz_with_binary(self):
 
         for i in range(1, 101):
-            print(self.convert_to_binary("FizzBuzz")) if i % 3 == 0 and i % 5 == 0 \
+            print(self.convert_to_binary("FizzBuzz")) if i % 15 == 0 \
                 else print(self.convert_to_binary("Fizz")) if i % 3 == 0 \
                 else print(self.convert_to_binary("Buzz")) if i % 5 == 0 \
                 else print(self.convert_to_binary(str(i)))
@@ -38,7 +58,6 @@ class FizzBuzz:
 
     # Fizzbuzz with range
     def fizz_with_range(self):
-
         for i in range(1, 101):
             self.print_fizz_buzz(i)
             i += 1
@@ -51,15 +70,7 @@ class FizzBuzz:
             self.print_fizz_buzz(i)
             i += 1
 
-    # Fizzbuzz with function calls
-    def fizz_with_function_calls(self):
-
-        i = 1
-        while i < 101:
-            self.print_fizz_buzz(i)
-            i += 1
-
-    # Function calls is stacked on stack and when all of them realised
+    # Function calls is stacked on stack after comliting all calls it realises them
     # fizz_buzz with recursion
     def fizz_with_recursion(self, start):
 
@@ -73,15 +84,44 @@ class FizzBuzz:
     # Print answer of fizz buzz
     def print_fizz_buzz(self, i):
 
-        print("FizzBuzz") if i % 3 == 0 and i % 5 == 0 else print("Fizz") \
-            if i % 3 == 0 else print("Buzz") if i % 5 == 0 else print(i)
+        print("FizzBuzz") if i % 15 == 0 \
+            else print("Fizz") if i % 3 == 0 \
+            else print("Buzz") if i % 5 == 0 \
+            else print(i)
 
     # Converts answers from fizzbuzz too binary
     def convert_to_binary(self, convertable):
 
         return ' '.join(format(ord(x), 'b') for x in convertable)
 
+#function not in class
+def fizz_with_range():
+    for i in range(1, 101):
+        print("FizzBuzz") if i % 15 == 0 \
+            else print("Fizz") if i % 3 == 0 \
+            else print("Buzz") if i % 5 == 0 \
+            else print(i)
+        i += 1
 
-new_buzz = FizzBuzz(True)
+new_buzz = FizzBuzz()
+#1. Function call fizzbuzz
+#fizz_with_range()
+#2. Class initiation fizzbuzz
+#$new_buzz = FizzBuzz(False)
+#3 Class within class fizzbuzz
+#new_buzz.Fizzbuzzer(True)
+#4.With range
+#new_buzz.fizz_with_range()
+#5.With while
+#new_buzz.fizz_with_while()
+#6.With Binary
+#new_buzz.fizz_with_binary()
+#7.Static fizz buzz method
+#new_buzz.fizz_with_static_method()
+#8.Class method
+#new_buzz.fizz_with_class_method()
+#9. Fizz with recursion
+#new_buzz.fizz_with_recursion()
 
-
+#new_buzz.fizz_argument
+new_buzz.fizz_argument
